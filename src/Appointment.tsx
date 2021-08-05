8cb51da12b3bc0ce6bf919733b8b3461d8d30fee
Emmanuel Ochoa <eorivera95@gmail.com>
2021-08-05 13:05:19 -0600

    Component AppointmentsDayView with tests

--- src/Appointment.tsx
+++ src/Appointment.tsx
@@ -6,6 +6,30 @@ interface Props {
   };
 }
 
+interface IAppointmentsDayViewProps {
+  appointments: { startsAt: number }[];
+}
+
+const appointmentTimeOfDay = (startsAt: number) => {
+  const [h, m] = new Date(startsAt).toTimeString().split(":");
+  return `${h}:${m}`;
+};
 export const Appointment: FC<Props> = ({ customer: { firstName } }) => {
   return <div>{firstName}</div>;
+};
+
+export const AppointmentsDayView: FC<IAppointmentsDayViewProps> = ({
+  appointments,
+}) => {
+  return (
+    <div id="appointmentsDayView">
+      <ol>
+        {appointments.map((appointment) => (
+          <li key={appointment.startsAt}>
+            {appointmentTimeOfDay(appointment.startsAt)}
+          </li>
+        ))}
+      </ol>
+    </div>
+  );
 };
--- test/Appointment.test.tsx
+++ test/Appointment.test.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-import { Appointment } from "../src/Appointment";
+import { Appointment, AppointmentsDayView } from "../src/Appointment";
 // describe defines a test suite, which is simply a set of tests.
 describe("Appointment", () => {
   let customer;
@@ -26,5 +26,41 @@ describe("Appointment", () => {
     customer = { firstName: "Jordan" };
     ReactDOM.render(<Appointment customer={customer} />, container);
     expect(container.textContent).toMatch("Jordan");
+  });
+});
+
+describe("AppointmentsDayView", () => {
+  let container: HTMLDivElement;
+  const today = new Date();
+  const appointments = [
+    { startsAt: today.setHours(12, 0) },
+    { startsAt: today.setHours(13, 0) },
+  ];
+
+  beforeEach(() => {
+    container = document.createElement("div");
+  });
+
+  const render = (component: JSX.Element) => {
+    ReactDOM.render(component, container);
+  };
+
+  it("renders a div with the right id", () => {
+    render(<AppointmentsDayView appointments={[]} />);
+    expect(container.querySelector("div#appointmentsDayView")).not.toBeNull();
+  });
+
+  it("renders multiple appointments in an ol element", () => {
+    render(<AppointmentsDayView appointments={appointments} />);
+    expect(container.querySelector("ol")).not.toBeNull();
+    expect(container.querySelector("ol")?.children).toHaveLength(2);
+  });
+
+  it("renders each appointment in a li", () => {
+    render(<AppointmentsDayView appointments={appointments} />);
+
+    expect(container.querySelectorAll("li")).toHaveLength(2);
+    expect(container.querySelectorAll("li")[0].textContent).toEqual("12:00");
+    expect(container.querySelectorAll("li")[1].textContent).toEqual("13:00");
   });
 });
